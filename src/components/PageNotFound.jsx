5a1e05acc61fe3602dcd051ce77de32de2bdbf53
7Flafleur <7nb@hotmail.fr>
2024-05-13 21:18:33 +0200

    404 ok

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,11 +2,11 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function PageNotFound(){
     return(
-        <div>
+        <div  id="p404"> 
             <h1 id="notFoundTitle" className="Page_not_found">404</h1>
             <h2 id="notFoundSubtitle">Oups! La page que vous demandez n'existe pas!</h2>
 
-            <Link to="/">Retourner sur la page d’accueil</Link>
+            <Link id="linkhome" to="/">Retourner sur la page d’accueil</Link>
         </div>
     )
 }
