8ef5cf25742133c739ccce978aa4cabae861dab0
7Flafleur <7nb@hotmail.fr>
2024-05-15 13:18:37 +0200

    slideshow functional

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,5 +1,53 @@
+import { useParams } from "react-router-dom"
+import { useState } from "react";
+import { getLogement } from "../api";
+import previous from "../assets/Vector left.png"
+import next from "../assets/Vector right.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft,faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
+
+
 export default function Carousel(props){
+
+    const [activeIndex, setActiveIndex] =useState(0);
+
+    const {Id} =useParams();
+
+    const logement= getLogement(Id);
+
+
+const pictures=logement.pictures;
+
+function previousItem() {
+    setActiveIndex(prevActiveIndex => prevActiveIndex - 1 < 0 ? pictures.length - 1 : prevActiveIndex - 1);
+  }
+
+  function nextItem() {
+    setActiveIndex(prevActiveIndex => (prevActiveIndex + 1) % pictures.length);
+  }
+
     return(
-        <div className="carousel">This is a carousel for property {props.id}</div>
+        <ul className="carousel">
+            
+            <button className=" btn previous" >
+            <FontAwesomeIcon icon={faChevronLeft} onClick={previousItem} />
+                </button>
+            <button className="btn next" >
+                
+                <FontAwesomeIcon icon={faChevronRight} onClick={nextItem} />
+                </button>
+
+    {
+        pictures.map((picture,index)=>(
+            <li key={index}>
+                <figure className={`carouselItem ${index === activeIndex ? "active" : ""}`}>
+                    <img className="picture" src={picture} alt={`${logement.title}`} />
+    
+                </figure>
+            </li>
+        ))
+    }
+            </ul>
     )
 }
